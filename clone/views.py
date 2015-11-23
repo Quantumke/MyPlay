37216e8aa9e7c302e9e3be8539c9490bc3be2dca
Quantumke <nguruben@gmail.com>
2015-11-23 20:22:06 +0300

    Render succesfull

--- clone/views.py
+++ clone/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
+from clone.models import Dplay
 
 # Create your views here.
+
+def index(request):
+	return render_to_response('index.html',{
+	'Dplay':Dplay.objects.all()
+	})
