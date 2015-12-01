422dc133df6c201ca643d9772b981871b8739cab
Quantumke <nguruben@gmail.com>
2015-12-01 12:39:44 +0300

    added a new view

--- clone/views.py
+++ clone/views.py
@@ -21,6 +21,11 @@ def chinese(request):
 	return render_to_response('chinese.html',{
 	'chinese':Dplay.objects.filter(channel='chinese')
 	})
+def hindi(request):
+        return render_to_response('hindi.html',{
+        'hindi':Dplay.objects.filter(channel='hindi')
+        })
+
 def view_more(request, slug):
 	return render_to_response('single.html',{
 	'post':get_object_or_404(Dplay, slug=slug)
