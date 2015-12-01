e0ad5e7492cf6e021603449301359cf4e6523bdb
Quantumke <nguruben@gmail.com>
2015-12-01 11:17:03 +0300

    templated english movies

--- clone/views.py
+++ clone/views.py
@@ -11,10 +11,15 @@ def index(request):
 	'popular':Dplay.objects.filter(uploaded_by='Ben Kiboko')[:4],
 	'recommended':Dplay.objects.filter(views__gt=1000)[:4],
 	'sport':Dplay.objects.filter(category='Sport')[:4],
-	'blockbusters':Dplay.objects.filter(category='blockbuster')
+	'english':Dplay.objects.filter(channel='English')
+	})
+def english(request):
+ 	return render_to_response('english.html',{
+	'english':Dplay.objects.filter(channel='English')
 	})
 def view_more(request, slug):
 	return render_to_response('single.html',{
 	'post':get_object_or_404(Dplay, slug=slug)
 	})
+
 
